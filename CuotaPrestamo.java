9349c9a4688a45f6c62a52183ddd93712cee81fd
Alipser <romarioajb@outlook.com>
2022-06-21 19:55:32 -0500

    credit fee

--- CuotaPrestamo.java
+++ CuotaPrestamo.java
@@ -7,9 +7,11 @@ public class CuotaPrestamo {
         System.out.print("Ingrese el monto a prestar:  ");
         double monto =sc.nextDouble();
         System.out.print("Ingrese tasa de prestamo deseada:   ");
-        double tasa= sc.nextDouble();
-        double operation= monto*tasa;
-        System.out.println(operation);
+        double tasa= sc.nextDouble()/100;
+        System.out.print("Ingrese el numero de periodos a ser financiados:     ");
+        int period=sc.nextInt();
+        double cuota= monto*(Math.pow(1+tasa,period)*tasa)/(Math.pow(1+tasa,period)-1);
+        System.out.println("el valor de la cuota es: " + cuota);
         sc.close();
         
        
