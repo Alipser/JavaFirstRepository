9747b06a6b02ab01aea2325e7adf08f7299a4148
Alipser <romarioajb@outlook.com>
2022-06-24 16:43:19 -0500

    Cambio de Versión Java8

--- CreditFeeArgs.java
+++ CreditFeeArgs.java
@@ -1,12 +1,12 @@
 
 public class CreditFeeArgs {
     
-        public static void main(double[] args){
+        public static void main(String[] args){
             if((args.length>=3)){
                 System.out.println("Cuota de Prestamo");
-                double monto=args[0];
-                double tasa=args[1];
-                double period=args[2];
+                double monto=Double.parseDouble(args[0]);
+                double tasa=Double.parseDouble(args[1])/100;
+                double period=Double.parseDouble(args[2]);
                 double cuota= monto*(Math.pow(1+tasa,period)*tasa)/(Math.pow(1+tasa,period)-1);
                 System.out.println("el valor de la cuota es: " + cuota);
             }
